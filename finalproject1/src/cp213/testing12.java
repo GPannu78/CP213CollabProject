256944731d4aefd5669681e6859ae4b0aa2ea3a8
GPannu78 <thebossnoor2@gmail.com>
2025-11-11 20:35:54 -0500

    added files

---
+++ finalproject1/src/cp213/testing12.java
@@ -0,0 +1,20 @@
+/**
+ * @author Gurnoor Pannu
+ * @version 2025-11-11
+ */
+package cp213;
+
+/**
+ * 
+ */
+public class testing12 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.print("Hellow");
+    }
+
+}
