0ae6342c545fb5709a4b5853f90979f4a6ae03c3
GPannu78 <thebossnoor2@gmail.com>
2025-11-13 14:47:05 -0500

    add

--- finalproject1/src/cp213/McQuestions.java
+++ finalproject1/src/cp213/McQuestions.java
@@ -7,6 +7,15 @@ package cp213;
 /**
  * 
  */
-public class McQuestions {
+public class McQuestions extends Questions {
+    private String expl;
 
+    public McQuestions(String question, String[] options, int indexans, String expl) {
+	super(question, options, indexans);
+	this.expl = expl;
+    }
+
+    public String getexpl() {
+	return expl;
+    }
 }
--- finalproject1/src/cp213/Questions.java
+++ finalproject1/src/cp213/Questions.java
@@ -8,5 +8,25 @@ package cp213;
  * 
  */
 public class Questions {
+    protected String question;
+    protected String[] options;
+    protected int indexans;
+
+    public Questions(String question, String[] options, int indexans) {
+	this.question = question;
+	this.options = options;
+	this.indexans = indexans;
+    }
+
+    public void displayq() {
+	System.out.println(question);
+	for (int i = 0; i < options.length; i++) {
+	    System.out.println("Option " + (i + 1) + ": " + options[i]);
+	}
+    }
+
+    public boolean checkans(int ans) {
+	return indexans == ans;
+    }
 
 }
