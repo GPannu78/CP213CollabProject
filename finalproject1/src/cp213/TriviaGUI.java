6df682130e3b79f3abd9364c6b18eacfefa3cf86
GPannu78 <thebossnoor2@gmail.com>
2025-11-20 11:15:48 -0500

    added GUI class

---
+++ finalproject1/src/cp213/TriviaGUI.java
@@ -0,0 +1,16 @@
+/**
+ * @author Gurnoor Pannu
+ * @version 2025-11-20
+ */
+package cp213;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+public class TriviaGUI {
+    private int index = 0;
+    private ArrayList<McQuestions> q1;
+
+}
